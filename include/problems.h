975fb6291daa3cf3a90b5b3e8616a8b4ee9fee7f
Joachim <2715622397@qq.com>
2026-07-01 02:15:20 +0800

    判断101-200之间的素数，并输出个数

--- include/problems.h
+++ include/problems.h
@@ -16,5 +16,6 @@ void problem_009 (void);
 void problem_010 (void);
 // === AUTO_INSERT_DECLARE ===   ← 加这一行，作为声明插入位置
 void problem_011(void);
+void problem_012(void);
 
 #endif
---
+++ src/problem_012.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include "problems.h"
+
+bool isPrime(int n){
+    if ( n <= 1){
+        return false ;
+    }
+    else if ( n == 2){
+        return true;
+    }
+    else if ( n % 2 == 0){
+        return false;
+    }
+    for ( int i =3 ; i*i <= n; i+=2 ){
+        if ( n % i == 0){
+            return false;
+        } 
+        // return true ; 这里是错误的，不能再犯
+    }
+     return true ;
+}
+void problem_012(void)
+{
+    printf("===== 第12题：判断101-200之间的素数 =====\n");
+    int j,count=0;
+    for ( j =101;j <= 200;j++){
+        if (isPrime( j )){
+            printf("%d\n",j);
+            count++;
+        }
+    }
+    printf("一共有 %d个素数\n",count);
+}
